0ddd0405eef607a003b63ba546adad2877d1bb65
RodrigoChalco <chsrodrigo@gmail.com>
2025-09-25 11:59:21 -0400

    Se implementó la conexión a la Base de datos

--- src/conexionbd/conexion.java
+++ src/conexionbd/conexion.java
@@ -4,10 +4,28 @@
  */
 package conexionbd;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 /**
  *
  * @author Gusssss
  */
 public class conexion {
-    
+    private static final String URL = "jdbc:mysql://switchyard.proxy.rlwy.net:51254/sys";
+    private static final String USER = "root";
+    private static final String PASS = "JTuKQjVAmaHRdZVsVwrwiggkoVwYpIql";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASS);
+    }
+
+    // Método de prueba
+    public static void main(String[] args) {
+        try (Connection con = getConnection()) {
+            System.out.println("¡Conexión exitosa!");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
